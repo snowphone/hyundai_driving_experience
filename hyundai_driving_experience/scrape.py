d73345a7f0909d009e42ff919225c46cea0112a2
Junoh Moon <mjo970625@gmail.com>
2022-08-31 22:25:22 +0900

    Add notifiable flag

--- hyundai_driving_experience/scrape.py
+++ hyundai_driving_experience/scrape.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+from argparse import (
+    ArgumentParser,
+    Namespace,
+)
+from typing import Iterable
+
 from bs4 import BeautifulSoup
+from requests import post
 from requests_html import (
     HTML,
     HTMLResponse,
@@ -30,14 +37,28 @@ def get_schedules(html: HTML):
     return {it.strip().replace("\n", " ") for it in raw_strings}
 
 
-def main():
+def notify(texts: Iterable[str]):
+    data = "\n".join(texts).encode("utf-8")
+    post(
+        "https://ntfy.sixtyfive.me/hyundai_driving_experience",
+        data=data,
+    )
+    return
+
+
+def main(args: Namespace):
     resp = load_page()
 
     entries = get_schedules(resp.html)
 
     latest = History.get_latest()
 
-    print(*(entries - latest), sep="\n")
+    diff = entries - latest
+
+    if diff:
+        print(*diff, sep="\n")
+        if args.notify:
+            notify(diff)
 
     History.store(entries)
 
@@ -45,4 +66,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser.add_argument("--notify", action="store_true")
+
+    main(parser.parse_args())
